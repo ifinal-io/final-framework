b096cc9c887e4fc519006afdc30f9eef07eaaff3
likly <569553290@qq.com>
2018-09-26 20:59:58 +0800

    [A] add exception context

---
+++ final-context/final-context-exception/src/main/java/cn/com/likly/finalframework/context/exception/IException.java
@@ -0,0 +1,18 @@
+package cn.com.likly.finalframework.context.exception;
+
+import lombok.NonNull;
+
+/**
+ * @author likly
+ * @version 1.0
+ * @date 2018-09-26 20:57
+ * @since 1.0
+ */
+public interface IException {
+
+    @NonNull
+    Integer getCode();
+
+    @NonNull
+    String getMessage();
+}
