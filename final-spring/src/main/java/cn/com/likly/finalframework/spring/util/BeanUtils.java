269e71043610de9d0bad0a2ab5d692d38637d9cf
zhimin.guo <zhimin.guo@dmall.com>
2018-10-31 11:06:37 +0800

    [A] refactor BeanUtils

--- final-spring/src/main/java/cn/com/likly/finalframework/spring/util/BeanUtils.java
+++ final-spring/src/main/java/cn/com/likly/finalframework/spring/util/BeanUtils.java
@@ -8,6 +8,7 @@ import java.lang.annotation.Annotation;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @SuppressWarnings("unused")
 public interface BeanUtils {
@@ -21,12 +22,21 @@ public interface BeanUtils {
      */
     @SuppressWarnings("unchecked")
     static <T> List<T> findBeansByAnnotation(ApplicationContext applicationContext, Class<? extends Annotation> annotationType) {
+        return findAllBeansAnnotatedBy(applicationContext, annotationType)
+                .map(it -> (T) it)
+                .collect(Collectors.toList());
+    }
+
+    static Stream<String> findAllBeans(ApplicationContext applicationContext) {
+        Assert.isNull(applicationContext, "applicationContext must be not null!");
+        return Arrays.stream(BeanFactoryUtils.beanNamesForTypeIncludingAncestors(applicationContext, Object.class));
+    }
+
+    static Stream<Object> findAllBeansAnnotatedBy(ApplicationContext applicationContext, Class<? extends Annotation> annotationType) {
         Assert.isNull(applicationContext, "applicationContext must be not null!");
         Assert.isNull(annotationType, "annotationType must be not null!");
-        return Arrays.stream(BeanFactoryUtils.beanNamesForTypeIncludingAncestors(applicationContext, Object.class))
-                .filter(name -> applicationContext.findAnnotationOnBean(name, annotationType) != null)
-                .map(name -> (T) applicationContext.getBean(name))
-                .collect(Collectors.toList());
+        return findAllBeans(applicationContext).filter(name -> applicationContext.findAnnotationOnBean(name, annotationType) != null)
+                .map(applicationContext::getBean);
     }
 
 
