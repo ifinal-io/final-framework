2cdac75a5f8a6b11110594bb8c2e84c4bd371ce0
likly <likly@likly.com.cn>
2019-03-17 12:27:04 +0800

    [U] 添加地址信息

--- docs/assets/js/markdown.js
+++ docs/assets/js/markdown.js
@@ -22,6 +22,23 @@ function openHttpLinkInBlank() {
  * 替换页面中所有的<a href="*.md">的为<a href="*.html">
  */
 function formatAllLink() {
+
+    let ip = returnCitySN["cip"];
+    console.log(returnCitySN["cip"] + ',' + returnCitySN["cname"]);
+    let domain = "https://github.com/likly/final-framework/blob/master";
+    $.ajax({
+        url: 'http://ip.taobao.com/service/getIpInfo.php?ip=' + ip,
+        async: false,
+        success: function (result) {
+            console.log(result);
+            let countryId = result.data.country_id;
+            if (countryId == 'CN') {
+                domain = "https://coding.net/u/likly/p/final-framework/git/blob/master";
+            }
+
+        }
+    });
+
     $('a').each(function () {
         let github = "https://github.com/likly/final-framework/blob/master";
         let $a = $(this);
