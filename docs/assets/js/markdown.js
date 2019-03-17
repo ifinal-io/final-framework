9c463bbb88bf0e5671ec1c980245f585ad46d74d
likly <likly@likly.com.cn>
2019-03-17 12:51:03 +0800

    [U] 添加地址信息

--- docs/assets/js/markdown.js
+++ docs/assets/js/markdown.js
@@ -22,32 +22,15 @@ function openHttpLinkInBlank() {
  * 替换页面中所有的<a href="*.md">的为<a href="*.html">
  */
 function formatAllLink() {
-
-    let ip = returnCitySN["cip"];
-    console.log(returnCitySN["cip"] + ',' + returnCitySN["cname"]);
-    let domain = "https://github.com/likly/final-framework/blob/master";
-    $.ajax({
-        url: 'http://ip.taobao.com/service/getIpInfo.php?ip=' + ip,
-        async: false,
-        success: function (result) {
-            console.log(result);
-            let countryId = result.data.country_id;
-            if (countryId == 'CN') {
-                domain = "https://coding.net/u/likly/p/final-framework/git/blob/master";
-            }
-
-        }
-    });
-
+    let domain = "{{ site.domain }}";
     $('a').each(function () {
-        let github = "https://github.com/likly/final-framework/blob/master";
         let $a = $(this);
         let href = $a.attr('href');
         if (href !== undefined && href.indexOf('.md') !== -1) {
             console.log(href);
             $a.attr('href', href.substr(0, href.length - 3).concat('.html'));
         } else if (href !== undefined && href.indexOf('.java') !== -1) {
-            $a.attr('href', github + href);
+            $a.attr('href', domain + href);
         }
     });
 }
