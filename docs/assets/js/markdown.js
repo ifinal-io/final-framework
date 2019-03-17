7b089ccb62b2456ad200a56c9901d6c3f7642d89
likly <likly@likly.com.cn>
2019-03-17 13:03:30 +0800

    [U] 添加 format-href.jekyll

--- docs/assets/js/markdown.js
+++ docs/assets/js/markdown.js
@@ -1,36 +1,9 @@
 $(document).ready(function () {
-    formatAllLink();
-    openHttpLinkInBlank();
     tableAddClass();
-
 });
 
 function tableAddClass() {
     $('table').each(function () {
         $(this).addClass('table table-striped table-bordered table-hover dataTable dtr-inline');
     })
-}
-
-//为超链接加上target='_blank'属性
-function openHttpLinkInBlank() {
-    $('a[href^="http"]').each(function () {
-        $(this).attr('target', '_blank');
-    });
-}
-
-/**
- * 替换页面中所有的<a href="*.md">的为<a href="*.html">
- */
-function formatAllLink() {
-    let domain = "{{ site.domain }}";
-    $('a').each(function () {
-        let $a = $(this);
-        let href = $a.attr('href');
-        if (href !== undefined && href.indexOf('.md') !== -1) {
-            console.log(href);
-            $a.attr('href', href.substr(0, href.length - 3).concat('.html'));
-        } else if (href !== undefined && href.indexOf('.java') !== -1) {
-            $a.attr('href', domain + href);
-        }
-    });
 }
