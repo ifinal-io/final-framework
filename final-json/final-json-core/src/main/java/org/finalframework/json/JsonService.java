5a1a61d8fecf431a60bebb084256a598c8239e56
likly <likly@likly.com.cn>
2019-04-08 10:22:34 +0800

    [U] refactor json service

--- final-json/final-json-core/src/main/java/org/finalframework/json/JsonService.java
+++ final-json/final-json-core/src/main/java/org/finalframework/json/JsonService.java
@@ -4,13 +4,18 @@ import lombok.NonNull;
 
 import java.lang.reflect.Type;
 import java.util.Collection;
+import java.util.List;
+import java.util.Set;
 
 /**
+ * Json 序列化与反序列化服务接口
+ *
  * @author likly
  * @version 1.0
  * @date 2018-09-26 21:24
  * @since 1.0
  */
+@SuppressWarnings("unchecked")
 public interface JsonService {
 
     /**
@@ -36,6 +41,22 @@ public interface JsonService {
 
     default <T> T toObject(@NonNull String json, @NonNull Type typeOfT, @NonNull Class<?> view) throws Throwable {
         return toObject(json, typeOfT);
+    }
+
+    default <E> List<E> toList(@NonNull String json, @NonNull Class<E> elementClass) throws Throwable {
+        return toCollection(json, List.class, elementClass);
+    }
+
+    default <E> List<E> toList(@NonNull String json, @NonNull Class<E> elementClass, @NonNull Class<?> view) throws Throwable {
+        return toCollection(json, List.class, elementClass, view);
+    }
+
+    default <E> Set<E> toSet(@NonNull String json, @NonNull Class<E> elementClass) throws Throwable {
+        return toCollection(json, Set.class, elementClass);
+    }
+
+    default <E> Set<E> toSet(@NonNull String json, @NonNull Class<E> elementClass, @NonNull Class<?> view) throws Throwable {
+        return toCollection(json, Set.class, elementClass, view);
     }
 
     <E, T extends Collection<E>> T toCollection(@NonNull String json, @NonNull Class<T> collectionClass, @NonNull Class<E> elementClass) throws Throwable;
