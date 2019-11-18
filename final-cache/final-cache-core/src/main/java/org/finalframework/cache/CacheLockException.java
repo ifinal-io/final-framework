a64b75a0b2ac42db08a8ffdbf2a4399bcb151401
likly <likly@ilikly.com>
2019-11-18 09:54:52 +0800

    [U] update cache lock exception

--- final-cache/final-cache-core/src/main/java/org/finalframework/cache/CacheLockException.java
+++ final-cache/final-cache-core/src/main/java/org/finalframework/cache/CacheLockException.java
@@ -1,15 +1,21 @@
 package org.finalframework.cache;
 
 
+import org.finalframework.data.exception.ForbiddenException;
+
 /**
  * @author likly
  * @version 1.0
  * @date 2019-03-11 03:18:19
  * @since 1.0
  */
-public class CacheLockException extends CacheException {
+public class CacheLockException extends ForbiddenException {
 
     public CacheLockException(String message) {
         super(message);
+    }
+
+    public CacheLockException(Integer code, String message) {
+        super(code, message);
     }
 }
