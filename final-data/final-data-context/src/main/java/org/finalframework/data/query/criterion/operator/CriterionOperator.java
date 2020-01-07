03357ee65de170bbdcd3644bec177352623273e1
likly <likly@ilikly.com>
2020-01-07 17:00:07 +0800

    [U] update criterion

--- final-data/final-data-context/src/main/java/org/finalframework/data/query/criterion/operator/CriterionOperator.java
+++ final-data/final-data-context/src/main/java/org/finalframework/data/query/criterion/operator/CriterionOperator.java
@@ -10,24 +10,76 @@ import org.springframework.lang.NonNull;
  */
 public interface CriterionOperator {
 
+    /**
+     * IS NULL
+     */
+    CriterionOperator NULL = new DefaultCriterionOperator("NULL");
+
+    /**
+     * IS NOT NULL
+     */
+    CriterionOperator NOT_NULL = new DefaultCriterionOperator("NOT_NULL");
+
+    /**
+     * =
+     */
     CriterionOperator EQUAL = new DefaultCriterionOperator("EQUAL");
+
+    /**
+     * !=
+     */
     CriterionOperator NOT_EQUAL = new DefaultCriterionOperator("NOT_EQUAL");
+
+    /**
+     * >
+     */
     CriterionOperator GREATER_THAN = new DefaultCriterionOperator("GREATER_THAN");
+
+    /**
+     * >=
+     */
     CriterionOperator GREATER_THAN_EQUAL = new DefaultCriterionOperator("GREATER_THAN_EQUAL");
+
+    /**
+     * <
+     */
     CriterionOperator LESS_THAN = new DefaultCriterionOperator("LESS_THAN");
+
+    /**
+     * <=
+     */
     CriterionOperator LESS_THAN_EQUAL = new DefaultCriterionOperator("LESS_THAN_EQUAL");
 
+    /**
+     * IN
+     */
     CriterionOperator IN = new DefaultCriterionOperator("IN");
+
+    /**
+     * NOT IN
+     */
     CriterionOperator NOT_IN = new DefaultCriterionOperator("NOT_IN");
 
+    /**
+     * LIKE
+     */
     CriterionOperator LIKE = new DefaultCriterionOperator("LIKE");
+
+    /**
+     * NOT LIKE
+     */
     CriterionOperator NOT_LIKE = new DefaultCriterionOperator("NOT_LIKE");
 
+    /**
+     * BETWEEN AND
+     */
     CriterionOperator BETWEEN = new DefaultCriterionOperator("BETWEEN");
+
+    /**
+     * NOT BETWEEN AND
+     */
     CriterionOperator NOT_BETWEEN = new DefaultCriterionOperator("NOT_BETWEEN");
 
-    CriterionOperator NULL = new DefaultCriterionOperator("NULL");
-    CriterionOperator NOT_NULL = new DefaultCriterionOperator("NOT_NULL");
 
     @NonNull
     String getName();
