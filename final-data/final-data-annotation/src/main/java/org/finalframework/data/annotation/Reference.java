94e7fbcb23eba153bf32637b38288c953153417c
sli <sli@thoughtworks.com>
2020-03-17 00:03:47 +0800

    [A] update Reference

--- final-data/final-data-annotation/src/main/java/org/finalframework/data/annotation/Reference.java
+++ final-data/final-data-annotation/src/main/java/org/finalframework/data/annotation/Reference.java
@@ -5,14 +5,19 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import java.lang.reflect.Field;
 import org.finalframework.data.annotation.enums.ReferenceMode;
 import org.springframework.core.annotation.AliasFor;
 import org.springframework.data.annotation.Persistent;
 
 /**
+ * Marked the {@link Field} is a {@literal reference} column, and the {@linkplain Class type} of {@link Field} must
+ * {@literal implements} the {@link IEntity interface}.
+ *
  * @author likly
  * @version 1.0
  * @date 2018-10-15 15:14
+ * @see IEntity
  * @since 1.0
  */
 @Column
