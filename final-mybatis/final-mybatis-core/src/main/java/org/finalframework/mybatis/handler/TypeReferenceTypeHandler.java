95ba122c6e27df1a33539f3e0338007a78984c3b
likly <likly@ilikly.com>
2020-03-24 20:28:26 +0800

    [U] add TypeReferenceTypeHandler

---
+++ final-mybatis/final-mybatis-core/src/main/java/org/finalframework/mybatis/handler/TypeReferenceTypeHandler.java
@@ -0,0 +1,24 @@
+package org.finalframework.mybatis.handler;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import org.apache.ibatis.type.BaseTypeHandler;
+
+/**
+ * @author sli
+ * @version 1.0
+ * @date 2020-03-24 20:17:38
+ * @since 1.0
+ */
+public abstract class TypeReferenceTypeHandler<T> extends BaseTypeHandler<T> {
+
+    private final Type type;
+
+    public TypeReferenceTypeHandler() {
+        this.type = ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
+    }
+
+    protected Type getType() {
+        return type;
+    }
+}
