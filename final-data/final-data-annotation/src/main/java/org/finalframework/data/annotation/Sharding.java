e6d2deb72207ce4bc04e567564bf0021661bf28c
likly <likly@ilikly.com>
2020-04-16 23:55:00 +0800

    [U] refactor Sharding annotation

--- final-data/final-data-annotation/src/main/java/org/finalframework/data/annotation/Sharding.java
+++ final-data/final-data-annotation/src/main/java/org/finalframework/data/annotation/Sharding.java
@@ -4,7 +4,6 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import org.springframework.core.annotation.AliasFor;
 
 /**
  *
@@ -13,13 +12,8 @@ import org.springframework.core.annotation.AliasFor;
  * @date 2020-03-19 09:11:44
  * @since 1.0
  */
-@Column
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Sharding {
-    @AliasFor("name")
-    String value() default "";
 
-    @AliasFor("value")
-    String name() default "";
 }
