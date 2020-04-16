fac27d8bda0a0e55616ce51332a7f34568002703
likly <likly@ilikly.com>
2020-04-16 23:58:16 +0800

    [U] add Property docs

--- final-data/final-data-context/src/main/java/org/finalframework/data/mapping/Property.java
+++ final-data/final-data-context/src/main/java/org/finalframework/data/mapping/Property.java
@@ -1,10 +1,15 @@
 package org.finalframework.data.mapping;
 
-import org.finalframework.data.annotation.*;
-import org.springframework.data.mapping.PersistentProperty;
-
-import javax.validation.constraints.NotNull;
 import java.lang.annotation.Annotation;
+import javax.validation.constraints.NotNull;
+import org.finalframework.data.annotation.Default;
+import org.finalframework.data.annotation.Final;
+import org.finalframework.data.annotation.ReadOnly;
+import org.finalframework.data.annotation.Sharding;
+import org.finalframework.data.annotation.Transient;
+import org.finalframework.data.annotation.Virtual;
+import org.finalframework.data.annotation.WriteOnly;
+import org.springframework.data.mapping.PersistentProperty;
 
 
 /**
@@ -22,33 +27,38 @@ public interface Property extends PersistentProperty<Property> {
     String getColumn();
 
     /**
-     * @return true if don't have {@link Transient} annotation and have {@link Default} annotaion.
+     * @return true if don't have {@link Transient} annotation and have {@link Default} annotation.
      * @see Default
      */
     boolean isDefault();
 
     /**
+     * @return {@code true} if the {@link Property} annotated by {@link Final} annotation.
      * @see Final
      */
     boolean isFinal();
 
     /**
+     * @return {@code true} if the {@link Property} annotated by {@link Virtual} annotation.
      * @see Virtual
      */
     boolean isVirtual();
 
     /**
+     * @return {@code true} if this {@link Property} is annotated by {@link Sharding} annotation.
      * @see Sharding
      */
     boolean isSharding();
 
     /**
+     * @return {@code true} if this {@link Property} is annotated by {@link ReadOnly} annotation.
      * @see ReadOnly
      */
     boolean isReadOnly();
 
 
     /**
+     * @return {@code true} if this {@link Property} is annotated by {@link WriteOnly} annotation.
      * @see WriteOnly
      */
     boolean isWriteOnly();
