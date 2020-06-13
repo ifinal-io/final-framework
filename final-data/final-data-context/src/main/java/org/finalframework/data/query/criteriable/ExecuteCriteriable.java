fa7d41318b5c7a669bec22f48de81330991a4426
likly <likly@ilikly.com>
2020-06-13 17:19:57 +0800

    [U] update copyright

--- final-data/final-data-context/src/main/java/org/finalframework/data/query/criteriable/ExecuteCriteriable.java
+++ final-data/final-data-context/src/main/java/org/finalframework/data/query/criteriable/ExecuteCriteriable.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2018-2020.  the original author or authors.
+ *  <p>
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  <p>
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *  <p>
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
 package org.finalframework.data.query.criteriable;
 
 import org.finalframework.data.query.criterion.CriterionTarget;
@@ -14,9 +31,9 @@ import org.finalframework.data.query.criterion.function.MathFunction;
  * @since 1.0
  */
 public interface ExecuteCriteriable<V, R> extends
-    DateFunction<CriterionTarget<CriterionFunction>>,
-    MathFunction<V, CriterionTarget<CriterionFunction>>,
-    LogicFunction<V, CriterionTarget<CriterionFunction>>,
-    JsonFunction<V, CriterionTarget<CriterionFunction>> {
+        DateFunction<CriterionTarget<CriterionFunction>>,
+        MathFunction<V, CriterionTarget<CriterionFunction>>,
+        LogicFunction<V, CriterionTarget<CriterionFunction>>,
+        JsonFunction<V, CriterionTarget<CriterionFunction>> {
 
 }
