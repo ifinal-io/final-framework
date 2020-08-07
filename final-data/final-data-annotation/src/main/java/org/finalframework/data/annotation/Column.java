10ae1f1436d448a72f84969eec7491bfc663faab
likly <likly@ilikly.com>
2020-08-07 16:51:04 +0800

    [U] update docs

--- final-data/final-data-annotation/src/main/java/org/finalframework/data/annotation/Column.java
+++ final-data/final-data-annotation/src/main/java/org/finalframework/data/annotation/Column.java
@@ -42,7 +42,7 @@ import org.springframework.data.annotation.Persistent;
  * @see Transient
  * @since 1.0
  */
-@Target({ElementType.FIELD, ElementType.ANNOTATION_TYPE})
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.ANNOTATION_TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 @Persistent
