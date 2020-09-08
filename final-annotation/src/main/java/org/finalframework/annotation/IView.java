65e9a6d5d38cb953c02df2c504e88e83ed4fcffe
likly <likly@ilikly.com>
2020-09-08 16:31:36 +0800

    [A] update

--- final-annotation/src/main/java/org/finalframework/annotation/IView.java
+++ final-annotation/src/main/java/org/finalframework/annotation/IView.java
@@ -21,6 +21,7 @@ package org.finalframework.annotation;
  * @author likly
  * @version 1.0
  * @date 2020-04-01 11:35:26
+ * @see org.finalframework.annotation.data.View
  * @since 1.0
  */
 public interface IView {
---
+++ final-annotation/src/main/java/org/finalframework/annotation/data/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Use {@link java.lang.annotation.Annotation} mark the entity to mapping the table of database.
+ *
+ * @author likly
+ * @version 1.0
+ * @date 2020/9/8 16:24:26
+ * @since 1.0
+ */
+package org.finalframework.annotation.data;
