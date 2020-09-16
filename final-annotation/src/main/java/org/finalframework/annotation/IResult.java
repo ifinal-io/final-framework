475cf9500eae19361efa05f4bb42f8441b33a3d4
likly <likly@ilikly.com>
2020-09-16 17:51:29 +0800

    [A] docs

--- final-annotation/src/main/java/org/finalframework/annotation/IResult.java
+++ final-annotation/src/main/java/org/finalframework/annotation/IResult.java
@@ -5,13 +5,53 @@ import org.springframework.lang.NonNull;
 import org.springframework.lang.Nullable;
 
 /**
- * The interface of response holder which impl the {@linkplain IData data} holder.  when {@link #isSuccess()} return {@code true}
+ * The interface of response data holder which impl the {@linkplain IData data} holder.  when {@link #isSuccess()} return {@code true}
  * the response successful, otherwise use {@linkplain #getCode()} or {@linkplain #getMessage()} for more info. The code and message
  * could be throw by a {@linkplain Exception exception} which impl the interface of {@link IException}.
+ *
+ * <h4>Code Example</h4>
+ *
+ * <pre>
+ *     <code>
+ *         IResult result = getResult();
+ *         if(result.isSuccess()){
+ *              // deal data when success
+ *             Object data = result.getData();
+ *         }else{
+ *             // deal message when failure
+ *             logger.error("code={},message={}",result.getCode(),result.getMessage());
+ *         }
+ *     </code>
+ * </pre>
+ *
+ * <h4>Json Example</h4>
+ * <pre>
+ *     <code>
+ *         {
+ *              "status":null,
+ *              "description":null,
+ *              "code":null,
+ *              "message":null,
+ *              "data":null,
+ *              "pagination":null,
+ *              "trace":null,
+ *              "timestamp":1600249564385,
+ *              "duration":null,
+ *              "address":null,
+ *              "locale":null,
+ *              "timeZone":null,
+ *              "operator":null,
+ *              "view":null,
+ *              "exception":null,
+ *              "success":false
+ *          }
+ *     </code>
+ * </pre>
  *
  * @author likly
  * @version 1.0
  * @date 2020-08-25 13:29:07
+ * @see org.finalframework.annotation.result.Result
  * @since 1.0
  */
 public interface IResult<T> extends IData<T> {
---
+++ final-annotation/src/test/java/org/finalframework/data/annotation/ResultTest.java
@@ -0,0 +1,22 @@
+package org.finalframework.data.annotation;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.finalframework.annotation.result.Result;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author likly
+ * @version 1.0
+ * @date 2020/9/16 17:44:53
+ * @since 1.0
+ */
+class ResultTest {
+
+    @Test
+    void jsonTest() throws JsonProcessingException {
+        Result<Object> result = new Result<>();
+        System.out.println(new ObjectMapper().writeValueAsString(result));
+    }
+
+}
