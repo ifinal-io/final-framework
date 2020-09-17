6e19daada00565206d519708a969bdbb87010fc5
likly <likly@ilikly.com>
2020-09-17 13:41:30 +0800

    [A] docs

--- final-annotation/src/main/java/org/finalframework/annotation/IQuery.java
+++ final-annotation/src/main/java/org/finalframework/annotation/IQuery.java
@@ -1,6 +1,8 @@
 package org.finalframework.annotation;
 
 /**
+ * The marked {@code interface} of query to mark the target is a {@linkplain Object query}.
+ *
  * @author likly
  * @version 1.0
  * @date 2019-02-12 12:41:09
