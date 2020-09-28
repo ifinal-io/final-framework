93cf1e6ea2faf1e266349eb43cd10ffe2e156548
likly <likly@ilikly.com>
2020-09-28 18:29:13 +0800

    [A] docs

--- final-annotation/src/main/java/org/finalframework/annotation/data/UpperCase.java
+++ final-annotation/src/main/java/org/finalframework/annotation/data/UpperCase.java
@@ -1,9 +1,6 @@
 package org.finalframework.annotation.data;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 import java.lang.reflect.Field;
 
 
@@ -15,6 +12,7 @@ import java.lang.reflect.Field;
  * @date 2020-05-20 22:00:28
  * @since 1.0
  */
+@Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE, ElementType.FIELD})
 public @interface UpperCase {
