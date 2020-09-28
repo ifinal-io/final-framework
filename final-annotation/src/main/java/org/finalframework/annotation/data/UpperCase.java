1786b4a168d954b2cc14fbf701611d7706b796ce
likly <likly@ilikly.com>
2020-09-28 18:26:55 +0800

    [A] docs

--- final-annotation/src/main/java/org/finalframework/annotation/data/UpperCase.java
+++ final-annotation/src/main/java/org/finalframework/annotation/data/UpperCase.java
@@ -4,10 +4,11 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import java.lang.reflect.Field;
 
 
 /**
- * mark the {@link Class} or {@link java.lang.reflect.Field} use {@link String#toUpperCase()}.
+ * Annotated the name of {@link Class table} or {@link Field column} should be convert to upper case use {@link String#toUpperCase()}.
  *
  * @author likly
  * @version 1.0
