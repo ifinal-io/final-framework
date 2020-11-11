8d40d330539b26124184e19d7bb2acb9eec89cd2
likly <likly@ilikly.com>
2020-11-11 11:03:50 +0800

    [U] update docs

---
+++ final-framework/final-monitor/README.md
@@ -0,0 +1,13 @@
+---
+layout: post
+title: README
+subtitle: README
+description: README
+tags: []
+menus:
+    - README
+date: 2020-11-11 11:01:14 +800
+version: 1.0
+---
+    
+# final monitor    
--- final-framework/final-web/src/test/java/org/finalframework/web/HelloController.java
+++ final-framework/final-web/src/test/java/org/finalframework/web/HelloController.java
@@ -1,6 +1,7 @@
 package org.finalframework.web;
 
 import org.finalframework.annotation.data.YN;
+import org.finalframework.monitor.annotation.MonitorAction;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
     @RequestMapping("/hello")
+    @MonitorAction("访问HelloController")
     public String hello(String word) {
         return "hello " + word + "!";
     }
