ce0cb545cf8ec376112e5cafaa420c49c44f46b9
likly <likly@ilikly.com>
2021-01-06 09:27:58 +0800

    feature #6 move EnumSerializer.java to inner class for BeanEnumPropertySerializerModifier

--- final-framework/final-json/src/main/java/org/ifinal/finalframework/json/jackson/modifier/BeanEnumPropertySerializerModifier.java
+++ final-framework/final-json/src/main/java/org/ifinal/finalframework/json/jackson/modifier/BeanEnumPropertySerializerModifier.java
@@ -15,7 +15,6 @@ import java.util.Arrays;
 import java.util.Collection;
 import org.ifinal.finalframework.annotation.core.IEnum;
 import org.ifinal.finalframework.auto.annotation.AutoService;
-import org.ifinal.finalframework.json.jackson.serializer.EnumSerializer;
 
 /**
  * <h4>Feature:</h4>
@@ -163,6 +162,45 @@ public class BeanEnumPropertySerializerModifier extends AbsSimpleBeanPropertySer
             throws IOException {
 
             gen.writeString(value.name());
+        }
+
+    }
+
+    /**
+     * 枚举{@link IEnum}对象序列化器，将枚举序列化成一个Json对象。
+     *
+     * <pre>
+     *     <code>
+     *         {
+     *             "name" : "NAME",
+     *             "code" : 1,
+     *             "description" : "名称",
+     *         }
+     *     </code>
+     * </pre>
+     *
+     * @author likly
+     * @version 1.0.0
+     * @since 1.0.0
+     */
+    private static class EnumSerializer extends JsonSerializer<IEnum<?>> {
+
+        public static final EnumSerializer instance = new EnumSerializer();
+
+        @Override
+        public void serialize(final IEnum<?> value, final JsonGenerator gen, final SerializerProvider serializers)
+            throws IOException {
+
+            gen.writeStartObject();
+            gen.writeFieldName("code");
+            gen.writeObject(value.getCode());
+            if (value instanceof Enum) {
+                gen.writeFieldName("name");
+                gen.writeObject(((Enum<?>) value).name());
+            }
+            gen.writeFieldName("desc");
+            gen.writeObject(value.getDesc());
+            gen.writeEndObject();
         }
 
     }
