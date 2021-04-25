02a15b9ad372b187fe28f6826750d9a032863857
likly <likly@ilikly.com>
2021-04-25 14:30:50 +0800

    update copyright

--- final-aop/src/main/java/org/ifinal/finalframework/aop/DefaultAnnotationMethodInterceptor.java
+++ final-aop/src/main/java/org/ifinal/finalframework/aop/DefaultAnnotationMethodInterceptor.java
@@ -1,11 +1,31 @@
+/*
+ * Copyright 2020-2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package org.ifinal.finalframework.aop;
 
 import java.lang.reflect.Method;
 import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
+
 import org.aopalliance.intercept.MethodInvocation;
+
 import org.ifinal.finalframework.context.expression.MethodMetadata;
+
 import org.springframework.aop.framework.AopProxyUtils;
 import org.springframework.context.expression.AnnotatedElementKey;
 
