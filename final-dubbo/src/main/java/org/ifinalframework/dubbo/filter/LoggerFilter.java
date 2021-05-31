e4257f5a52ac3f809c769085942c16cc8499ce8f
likly <likly@ilikly.com>
2021-05-31 23:22:38 +0800

    add LoggerFilter

--- final-dubbo/src/main/java/org/ifinalframework/dubbo/filter/LoggerFilter.java
+++ final-dubbo/src/main/java/org/ifinalframework/dubbo/filter/LoggerFilter.java
@@ -1,6 +1,5 @@
 /*
  * Copyright 2020-2021 the original author or authors.
- *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -16,8 +15,16 @@
 
 package org.ifinalframework.dubbo.filter;
 
+import org.springframework.util.ReflectionUtils;
+
 import org.ifinalframework.dubbo.annotation.AutoFilter;
 
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import com.alibaba.fastjson.JSON;
 import org.apache.dubbo.common.constants.CommonConstants;
 import org.apache.dubbo.common.extension.Activate;
 import org.apache.dubbo.rpc.Filter;
@@ -44,13 +51,27 @@ public class LoggerFilter implements Filter {
     public Result invoke(final Invoker<?> invoker, final Invocation invocation) {
 
         final Logger logger = LoggerFactory.getLogger(invoker.getInterface());
+        final String service = invoker.getInterface().getSimpleName();
+        String methodName = invocation.getMethodName();
         try {
             if (logger.isInfoEnabled()) {
-                logger.info("#{}, args={}", invocation.getMethodName(), invocation.getArguments());
+
+                Method method = ReflectionUtils
+                    .findMethod(invoker.getInterface(), methodName, invocation.getParameterTypes());
+
+                Parameter[] parameters = method.getParameters();
+
+                Map<String, Object> args = new LinkedHashMap<>(parameters.length);
+
+                for (int i = 0; i < parameters.length; i++) {
+                    args.put(parameters[i].getName(), invocation.getArguments()[i]);
+                }
+
+                logger.info("{}#{}, args={}", service, methodName, JSON.toJSONString(args));
             }
             final Result result = invoker.invoke(invocation);
             if (logger.isInfoEnabled()) {
-                logger.info("#{}, result={}", invocation.getMethodName(), result.getValue());
+                logger.info("{}#{}, result={}", service, methodName, JSON.toJSONString(result.getValue()));
             }
             return result;
 
