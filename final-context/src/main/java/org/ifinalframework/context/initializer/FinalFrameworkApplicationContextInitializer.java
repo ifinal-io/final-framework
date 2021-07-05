5e28b7f5185e4e4bf929927fb70aa48759621ba6
likly <likly@ilikly.com>
2021-07-05 16:05:48 +0800

    update

--- final-context/src/main/java/org/ifinalframework/context/initializer/FinalFrameworkApplicationContextInitializer.java
+++ final-context/src/main/java/org/ifinalframework/context/initializer/FinalFrameworkApplicationContextInitializer.java
@@ -1,6 +1,5 @@
 /*
  * Copyright 2020-2021 the original author or authors.
- *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -39,6 +38,7 @@ public final class FinalFrameworkApplicationContextInitializer extends
 
     public FinalFrameworkApplicationContextInitializer() {
         super(FinalFramework.class);
+        logger.info("registered framework: {}", FinalFramework.class.getSimpleName());
     }
 
 }
