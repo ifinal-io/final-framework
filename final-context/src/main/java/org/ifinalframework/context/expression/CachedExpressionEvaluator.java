a076294de8e4d305ab7ce2923a2467cdaceef3ae
likly <likly@ilikly.com>
2021-07-15 13:06:15 +0800

    update

--- final-context/src/main/java/org/ifinalframework/context/expression/CachedExpressionEvaluator.java
+++ final-context/src/main/java/org/ifinalframework/context/expression/CachedExpressionEvaluator.java
@@ -1,6 +1,5 @@
 /*
  * Copyright 2020-2021 the original author or authors.
- *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -83,14 +82,9 @@ public abstract class CachedExpressionEvaluator {
      */
     protected Expression getExpression(Map<ExpressionKey, Expression> cache,
         AnnotatedElementKey elementKey, String expression) {
-
         ExpressionKey expressionKey = createKey(elementKey, expression);
-        Expression expr = cache.get(expressionKey);
-        if (expr == null) {
-            expr = getParser().parseExpression(expression, ParserContext.TEMPLATE_EXPRESSION);
-            cache.put(expressionKey, expr);
-        }
-        return expr;
+        return cache.computeIfAbsent(expressionKey,
+            key -> getParser().parseExpression(expression, ParserContext.TEMPLATE_EXPRESSION));
     }
 
     private ExpressionKey createKey(AnnotatedElementKey elementKey, String expression) {
