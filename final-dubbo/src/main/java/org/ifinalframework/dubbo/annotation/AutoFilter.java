b7a3c708e92302537f743c4ce685a71e20b42e5e
likly <likly@ilikly.com>
2021-08-13 13:36:55 +0800

    add SqlHelper.

--- final-dubbo/src/main/java/org/ifinalframework/dubbo/annotation/AutoFilter.java
+++ final-dubbo/src/main/java/org/ifinalframework/dubbo/annotation/AutoFilter.java
@@ -1,6 +1,5 @@
 /*
  * Copyright 2020-2021 the original author or authors.
- *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -16,6 +15,7 @@
 
 package org.ifinalframework.dubbo.annotation;
 
+import org.apache.dubbo.rpc.Filter;
 import org.ifinalframework.auto.service.annotation.AutoService;
 
 import java.lang.annotation.ElementType;
@@ -23,17 +23,17 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import org.apache.dubbo.rpc.Filter;
-
 /**
+ * Auto generate config file in {@code META-INF/dubbo/{service}}.
+ *
  * @author likly
  * @version 1.0.0
  * @since 1.0.0
  */
 @AutoService(
-    value = Filter.class,
-    path = "dubbo",
-    ignore = true
+        value = Filter.class,
+        path = "dubbo",
+        ignore = true
 )
 @Retention(RetentionPolicy.CLASS)
 @Target(ElementType.TYPE)
