09bbc599fc776b6a3ec6862f4ffc4dbb48982028
likly <likly@ilikly.com>
2021-11-15 11:03:58 +0800

    update docs

--- final-context/src/main/java/org/ifinalframework/context/listener/package-info.java
+++ final-context/src/main/java/org/ifinalframework/context/listener/package-info.java
@@ -1,6 +1,5 @@
 /*
  * Copyright 2020-2021 the original author or authors.
- *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -15,9 +14,59 @@
  */
 
 /**
+ * {@link org.springframework.context.ApplicationListener}
+ *
+ * <pre class="code">
+ * public class MyApplicationEventListener implements ApplicationListener<ApplicationReadyEvent>{
+ *
+ * }
+ * </pre>
+ *
+ * <h3>Registration</h4>
+ *
+ * <h4>Java API</h4>
+ * Use {@link org.springframework.boot.SpringApplication#addListeners(org.springframework.context.ApplicationListener[])} java api like this:
+ * <pre class="code">
+ * SpringApplication application = new SpringApplication();
+ * application.addListeners(new MyApplicationEventListener());
+ * application.run();
+ * </pre>
+ *
+ * <h4>Spring SPI</h4>
+ * Use <code>META-INF/spring.factories</code> Spring SPI.
+ * <pre class="code">
+ * # Application Listeners
+ * org.springframework.context.ApplicationListener=\
+ * org.springframework.boot.ClearCachesApplicationListener,\
+ * org.springframework.boot.builder.ParentContextCloserApplicationListener,\
+ * org.springframework.boot.context.FileEncodingApplicationListener,\
+ * org.springframework.boot.context.config.AnsiOutputApplicationListener,\
+ * org.springframework.boot.context.config.DelegatingApplicationListener,\
+ * org.springframework.boot.context.logging.LoggingApplicationListener,\
+ * org.springframework.boot.env.EnvironmentPostProcessorApplicationListener
+ * </pre>
+ *
+ * <h4>@</h4>
+ *
+ * <h4>{@link org.springframework.boot.context.event.EventPublishingRunListener}</h4>
+ *
+ * <ol>
+ *     <li>{@link org.springframework.boot.context.event.ApplicationStartingEvent}</li>
+ *     <li>{@link org.springframework.boot.context.event.ApplicationEnvironmentPreparedEvent}</li>
+ *     <li>{@link org.springframework.boot.context.event.ApplicationContextInitializedEvent}</li>
+ *     <li>{@link org.springframework.boot.context.event.ApplicationPreparedEvent}</li>
+ *     <li>{@link org.springframework.boot.context.event.ApplicationStartedEvent}</li>
+ *     <li>{@link org.springframework.boot.context.event.ApplicationReadyEvent}</li>
+ *     <li>{@link org.springframework.boot.context.event.ApplicationFailedEvent}</li>
+ * </ol>
+ *
  * @author likly
  * @version 1.0.0
+ * @see org.springframework.context.event.EventListener
+ * @see org.springframework.context.ApplicationListener
  * @see org.springframework.boot.SpringApplication#setListeners(java.util.Collection)
+ * @see org.springframework.boot.SpringApplicationRunListener
+ * @see org.springframework.boot.context.event.EventPublishingRunListener
  * @since 1.0.0
  */
 
