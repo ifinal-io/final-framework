120e9c5b87fed6874d8f2e0169033d0ee60c1f65
likly <likly@ilikly.com>
2021-12-21 11:04:47 +0800

    remove final-velocity module

--- final-core/src/main/java/org/ifinalframework/velocity/Velocities.java
+++ final-core/src/main/java/org/ifinalframework/velocity/Velocities.java
@@ -1,6 +1,5 @@
 /*
  * Copyright 2020-2021 the original author or authors.
- *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -16,15 +15,6 @@
 
 package org.ifinalframework.velocity;
 
-import java.beans.BeanInfo;
-import java.beans.Introspector;
-import java.beans.PropertyDescriptor;
-import java.io.StringWriter;
-import java.lang.reflect.Method;
-import java.util.Map;
-import java.util.Objects;
-import java.util.Properties;
-
 import ch.qos.logback.classic.Level;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.velocity.Template;
@@ -37,6 +27,15 @@ import org.apache.velocity.tools.ToolManager;
 import org.apache.velocity.tools.config.ConfigurationUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.beans.BeanInfo;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+import java.io.StringWriter;
+import java.lang.reflect.Method;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Properties;
 
 /**
  * A tool for {@code velocity} template language.
