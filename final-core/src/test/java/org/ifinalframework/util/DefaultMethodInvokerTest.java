f50b8dbf02ea3436b60df8ef2de20dcba1bc975f
likly <likly@ilikly.com>
2022-01-06 22:33:31 +0800

    add final-data-printer

--- final-core/src/test/java/org/ifinalframework/util/DefaultMethodInvokerTest.java
+++ final-core/src/test/java/org/ifinalframework/util/DefaultMethodInvokerTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2020-2022 the original author or authors.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.ifinalframework.util;
 
 import org.junit.jupiter.api.Test;
@@ -5,7 +20,7 @@ import org.junit.platform.commons.util.ReflectionUtils;
 
 import java.lang.reflect.Method;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * @author likly
@@ -40,6 +55,7 @@ class DefaultMethodInvokerTest {
     @Test
     void reflect(){
         assertEquals("1",Reflections.invokeMethod(this,"method",null,1));
-        assertEquals("1",Reflections.invokeMethod(this,"method",null,"1"));
+        assertEquals("1", Reflections.invokeMethod(this, "method", null, "1"));
+        assertEquals("1", Reflections.invokeMethod(this, "method", null, new Object[]{1}));
     }
 }
