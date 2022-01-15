1da84e66dd33a7a77aa4cd73b1ec55aed03da4ae
likly <zhimin.guo@dmall.com>
2022-01-15 16:27:51 +0800

    update

--- final-poi/src/main/java/org/ifinalframework/poi/function/ResultMapBiFunction.java
+++ final-poi/src/main/java/org/ifinalframework/poi/function/ResultMapBiFunction.java
@@ -1,70 +1,50 @@
 package org.ifinalframework.poi.function;
 
+import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.ifinalframework.poi.Headers;
 import org.ifinalframework.poi.TypeHandler;
-import org.ifinalframework.poi.TypeHandlerRegistry;
 import org.ifinalframework.poi.mapping.ResultMap;
 import org.ifinalframework.poi.mapping.ResultMapping;
+import org.ifinalframework.poi.type.ObjectTypeHandler;
+import org.springframework.beans.BeanWrapperImpl;
 
-import java.beans.BeanInfo;
-import java.beans.Introspector;
-import java.beans.PropertyDescriptor;
-import java.util.*;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.function.BiFunction;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 /**
  * @author likly
  * @version 1.2.4
  **/
+@RequiredArgsConstructor
 public class ResultMapBiFunction<T> implements BiFunction<Row, Headers, T> {
 
-    private final TypeHandlerRegistry registry;
+    private final TypeHandler<Object> typeHandler = new ObjectTypeHandler();
     private final ResultMap<T> resultMap;
-    private final Map<String, PropertyDescriptor> propertyDescriptors;
-
-    public ResultMapBiFunction(ResultMap<T> resultMap) {
-        this(new TypeHandlerRegistry(), resultMap);
-    }
-
-    @SneakyThrows
-    public ResultMapBiFunction(TypeHandlerRegistry registry, ResultMap<T> resultMap) {
-        this.registry = registry;
-        this.resultMap = resultMap;
-
-        if(resultMap.isMap()){
-            propertyDescriptors = null;
-        }else {
-            BeanInfo beanInfo = Introspector.getBeanInfo(resultMap.getJavaType());
-            propertyDescriptors = Arrays.stream(beanInfo.getPropertyDescriptors())
-                    .collect(Collectors.toMap(PropertyDescriptor::getName, Function.identity()));
-        }
-    }
 
     @Override
     @SneakyThrows
+    @SuppressWarnings("unchecked")
     public T apply(Row row, Headers headers) {
 
-        if(resultMap.isMap()){
-            Map<String,Object> map = new LinkedHashMap<>();
+        if (resultMap.isMap()) {
+            Map<String, Object> map = new LinkedHashMap<>();
 
-            headers.foreach((header,index) -> {
+            headers.foreach((header, index) -> {
                 Cell cell = row.getCell(index);
-                TypeHandler<Object> typeHandler = registry.getTypeHandler(Object.class);
                 Object value = Optional.ofNullable(cell).map(typeHandler::getValue).orElse(null);
-                map.put(header,value);
+                map.put(header, value);
             });
-
 
             return (T) map;
         }
 
-
-        T instance = resultMap.getJavaType().newInstance();
+        BeanWrapperImpl beanWrapper = new BeanWrapperImpl(resultMap.getJavaType());
 
         for (ResultMapping resultMapping : resultMap.getResultMappings()) {
 
@@ -76,17 +56,17 @@ public class ResultMapBiFunction<T> implements BiFunction<Row, Headers, T> {
 
             Cell cell = row.getCell(index);
 
-            PropertyDescriptor descriptor = propertyDescriptors.get(resultMapping.getProperty());
-            Class<?> javaType = descriptor.getPropertyType();
+            if(Objects.isNull(cell)){
+                continue;
+            }
 
-            TypeHandler<?> typeHandler = registry.getTypeHandler(javaType);
-            Object value = Optional.ofNullable(cell).map(typeHandler::getValue).orElse(null);
-            descriptor.getWriteMethod().invoke(instance, value);
-
+            TypeHandler<?> typeHandler = resultMapping.getTypeHandler();
+            Object value = typeHandler.getValue(cell);
+            beanWrapper.setPropertyValue(resultMapping.getProperty(), value);
 
         }
 
+        return (T) beanWrapper.getWrappedInstance();
 
-        return instance;
     }
 }
