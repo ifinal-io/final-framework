d3fcf83aa69094b271dd7798fa18c05deff35401
likly <zhimin.guo@dmall.com>
2022-03-01 14:32:30 +0800

    add JsonExceptionHandlerTest

--- final-web/src/main/java/org/ifinalframework/web/exception/result/JsonExceptionHandler.java
+++ final-web/src/main/java/org/ifinalframework/web/exception/result/JsonExceptionHandler.java
@@ -1,6 +1,5 @@
 /*
- * Copyright 2020-2021 the original author or authors.
- *
+ * Copyright 2020-2022 the original author or authors.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -16,14 +15,13 @@
 
 package org.ifinalframework.web.exception.result;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.lang.NonNull;
-import org.springframework.stereotype.Component;
-
 import org.ifinalframework.context.exception.result.ResultExceptionHandler;
 import org.ifinalframework.core.result.R;
 import org.ifinalframework.core.result.Result;
 import org.ifinalframework.json.JsonException;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
 
 /**
  * Json 异常处理器，将业务层抛出的{@link JsonException}异常包装成 {@link Result}返回给前端。
@@ -39,14 +37,12 @@ public class JsonExceptionHandler implements ResultExceptionHandler<JsonExceptio
 
     @Override
     public boolean supports(final @NonNull Throwable t) {
-
         return t instanceof JsonException;
     }
 
     @NonNull
     @Override
     public Result<?> handle(final JsonException e) {
-
         return R.failure(400, e.getMessage());
     }
 
