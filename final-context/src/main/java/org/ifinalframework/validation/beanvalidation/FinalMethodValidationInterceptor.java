cbf582dda7bf2e3891c125563b6a8c4eca160939
ilikly <ifinal.io@qq.com>
2023-03-16 14:09:17 +0800

    add ValidationGroupsProvider

---
+++ final-context/src/main/java/org/ifinalframework/validation/beanvalidation/FinalMethodValidationInterceptor.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2020-2023 the original author or authors.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.ifinalframework.validation.beanvalidation;
+
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.lang.NonNull;
+import org.springframework.util.ClassUtils;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.ObjectUtils;
+import org.springframework.validation.beanvalidation.MethodValidationInterceptor;
+
+import org.ifinalframework.validation.ValidationGroupsProvider;
+
+import org.aopalliance.intercept.MethodInvocation;
+
+/**
+ * FinalMethodValidationInterceptor.
+ *
+ * @author ilikly
+ * @version 1.5.0
+ * @since 1.5.0
+ */
+public class FinalMethodValidationInterceptor extends MethodValidationInterceptor {
+    private final ValidationGroupsProvider validationGroupsProvider;
+
+    public FinalMethodValidationInterceptor(ValidationGroupsProvider validationGroupsProvider) {
+        super();
+        this.validationGroupsProvider = validationGroupsProvider;
+    }
+
+    public FinalMethodValidationInterceptor(ValidatorFactory validatorFactory, ValidationGroupsProvider validationGroupsProvider) {
+        super(validatorFactory);
+        this.validationGroupsProvider = validationGroupsProvider;
+    }
+
+    public FinalMethodValidationInterceptor(Validator validator, ValidationGroupsProvider validationGroupsProvider) {
+        super(validator);
+        this.validationGroupsProvider = validationGroupsProvider;
+    }
+
+    @Override
+    @NonNull
+    protected Class<?>[] determineValidationGroups(@NonNull MethodInvocation invocation) {
+
+        final List<Class<?>> validationGroups = validationGroupsProvider.getValidationGroups(invocation.getMethod(), invocation.getThis(), invocation.getArguments());
+
+        if (CollectionUtils.isEmpty(validationGroups)) {
+            return super.determineValidationGroups(invocation);
+        }
+
+        final Class<?>[] determineValidationGroups = super.determineValidationGroups(invocation);
+
+        if (!ObjectUtils.isEmpty(determineValidationGroups)) {
+            validationGroups.addAll(Arrays.asList(determineValidationGroups));
+        }
+
+        return ClassUtils.toClassArray(validationGroups);
+    }
+}
