f60159e5e4162cc0b0179a09b209987061479de3
ilikly <ifinal.io@qq.com>
2023-04-12 15:06:33 +0800

    add JavaAssistProcessor

---
+++ final-context/src/main/java/org/ifinalframework/javassist/AbstractNestablePropertyAccessorJavaAssistProcessor.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2020-2023 the original author or authors.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.ifinalframework.javassist;
+
+
+import org.springframework.beans.AbstractNestablePropertyAccessor;
+import org.springframework.core.convert.TypeDescriptor;
+
+import org.ifinalframework.auto.service.annotation.AutoService;
+
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.CtField;
+import javassist.CtMethod;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * AbstractNestablePropertyAccessorJavaAssistProcessor.
+ *
+ * @author ilikly
+ * @version 1.5.0
+ * @see AbstractNestablePropertyAccessor
+ * @since 1.5.0
+ */
+@Slf4j
+@AutoService(JavaAssistProcessor.class)
+public class AbstractNestablePropertyAccessorJavaAssistProcessor implements JavaAssistProcessor {
+    @Override
+    public void process(ClassPool classPool) throws Throwable {
+
+        logger.info("start process org.springframework.beans.AbstractNestablePropertyAccessor");
+
+
+        final CtClass ctClass = classPool.get("org.springframework.beans.AbstractNestablePropertyAccessor");
+        final CtField list = CtField.make("private java.util.List beanTypeDescriptorFactories = org.springframework.core.io.support.SpringFactoriesLoader.loadFactories(org.ifinalframework.beans.BeanTypeDescriptorFactory.class,getClass().getClassLoader());", ctClass);
+        ctClass.addField(list);
+
+
+        /**
+         * @see AbstractNestablePropertyAccessor#newValue(Class, TypeDescriptor, String)
+         */
+        final CtMethod newValue = ctClass.getDeclaredMethod("newValue");
+        newValue.insertBefore("        for (int i = 0; i < beanTypeDescriptorFactories.size(); i++) {\n" +
+                "            org.ifinalframework.beans.BeanTypeDescriptorFactory beanTypeDescriptorFactory = beanTypeDescriptorFactories.get(i);\n" +
+                "            if (beanTypeDescriptorFactory.support(desc)) {\n" +
+                "                return beanTypeDescriptorFactory.create(type, desc);\n" +
+                "            }\n" +
+                "        }");
+
+        final Class<?> aClass = ctClass.toClass();
+
+        logger.info("process success org.springframework.beans.AbstractNestablePropertyAccessor");
+    }
+}
---
+++ final-context/src/main/java/org/ifinalframework/listener/JavaAssistApplicationStartingListener.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2020-2023 the original author or authors.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.ifinalframework.listener;
+
+import java.util.ServiceLoader;
+
+import org.springframework.boot.context.event.ApplicationStartingEvent;
+import org.springframework.context.ApplicationListener;
+
+import org.ifinalframework.auto.spring.factory.annotation.SpringFactory;
+import org.ifinalframework.javassist.JavaAssistProcessor;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * JavaAssistApplicationStartListener.
+ *
+ * @author ilikly
+ * @version 1.5.0
+ * @since 1.5.0
+ */
+@Slf4j
+@SpringFactory(ApplicationListener.class)
+public class JavaAssistApplicationStartingListener implements ApplicationListener<ApplicationStartingEvent> {
+    @Override
+    public void onApplicationEvent(ApplicationStartingEvent event) {
+        for (final JavaAssistProcessor javaAssistProcessor : ServiceLoader.load(JavaAssistProcessor.class)) {
+            logger.info("start {}", javaAssistProcessor);
+        }
+    }
+}
