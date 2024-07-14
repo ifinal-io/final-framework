58cd2e7fee9061aa62751efacccb017b029c6bd2
iimik <iimik@qq.com>
2024-07-14 10:17:27 +0800

    update Regexs

--- final-core/src/main/java/org/ifinalframework/util/Regexs.java
+++ final-core/src/main/java/org/ifinalframework/util/Regexs.java
@@ -19,7 +19,7 @@ package org.ifinalframework.util;
 import java.util.regex.Pattern;
 
 /**
- * Regexs.
+ * 正则匹配工具类.
  *
  * @author iimik
  * @version 1.0.0
@@ -30,6 +30,26 @@ public final class Regexs {
     private Regexs() {
     }
 
+    /**
+     * 校验给定的字符串是否满足正则校验
+     * @since 1.6.0
+     * @param regex 正则表达式
+     * @param input 要校验的字符串
+     * @return 是否满足正则
+     */
+    public static boolean matches(String regex, CharSequence input){
+        return Pattern.matches(regex, input);
+    }
+
+    /**
+     * @see #matches(String, CharSequence) 
+     * @param pattern
+     * @param input
+     * @return
+     * @deprecated 1.6.0 
+     * 
+     */
+    @Deprecated
     public static boolean matches(final Pattern pattern, final CharSequence input) {
         return pattern.matcher(input).matches();
     }
